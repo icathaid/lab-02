4bbeb99ce2397cd96e131fc2d990d423adf12355
James McDaniel <icathaid@gmail.com>
2018-10-06 21:21:02 -0700

    passing tests

--- objects/vehicle-class.js
+++ objects/vehicle-class.js
@@ -1,8 +1,37 @@
 'use strict';
 
-class Car {
-  constructor() {
+class Vehicle {
+  constructor(name, wheels){
+    this.name = name;
+    this.wheels = wheels;
+  }
+}
+class Car extends Vehicle {
+  constructor(props) {
+    super(props);
+    this.wheels = 4;
+  }
+  drive() {
+    return 'vrooooom!';
+  }
+  stop() {
+    return 'stopping';
+  }
+}
+class Motorcycle extends Vehicle {
+  constructor(props){
+    super(props);
+    this.wheels = 2;
+  }
+  wheelie(){
+    return 'wheeee!';
+  }
+  drive(){
+    return 'brmbrmbrmbrmbrm';
+  }
+  stop(){
+    return 'skrrrrt!';
   }
 }
 
-module.exports = Car;
+module.exports = {Car, Motorcycle};
